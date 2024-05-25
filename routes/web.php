0f0470b5c6ec72407bfd0a5966a013f2e262ead9
aguspranyoto <agusprnyt@gmail.com>
2024-05-25 14:26:39 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,27 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+class Post {
+    public static function all(){
+        return [
+            [
+                'id' => 1,
+                'slug' => 'memahami-quarter-life-crisis:-tantangan-dan-solusi',
+                'title' => 'Memahami Quarter Life Crisis: Tantangan dan Solusi',
+                'author' => 'Agus Pranyoto',
+                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos harum, culpa porro fuga laboriosam doloribus tenetur, perspiciatis qui id natus nam consectetur sapiente veritatis magnam maiores blanditiis exercitationem? Non, obcaecati?'
+            ],
+            [
+                'id' => 2,
+                'slug' => 'memahami-quarter-life-crisis:-tantangan-dan-solusi',
+                'title' => 'Memahami Quarter Life Crisis: Tantangan dan Solusi',
+                'author' => 'Agus Pranyoto',
+                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos harum, culpa porro fuga laboriosam doloribus tenetur, perspiciatis qui id natus nam consectetur sapiente veritatis magnam maiores blanditiis exercitationem? Non, obcaecati?'
+            ]
+        ];
+    }
+}
+
 Route::get('/', function () {
     return view('home',['header'=>'Home']);
 });
@@ -16,47 +37,16 @@ Route::get('/contact', function () {
 });
 
 Route::get('/blog', function () {
-    return view('blog',['header'=>'Blog', 'posts' => [
-        [
-            'id' => 1,
-            'slug' => 'memahami-quarter-life-crisis:-tantangan-dan-solusi',
-            'title' => 'Memahami Quarter Life Crisis: Tantangan dan Solusi',
-            'author' => 'Agus Pranyoto',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos harum, culpa porro fuga laboriosam doloribus tenetur, perspiciatis qui id natus nam consectetur sapiente veritatis magnam maiores blanditiis exercitationem? Non, obcaecati?'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'memahami-quarter-life-crisis:-tantangan-dan-solusi',
-            'title' => 'Memahami Quarter Life Crisis: Tantangan dan Solusi',
-            'author' => 'Agus Pranyoto',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos harum, culpa porro fuga laboriosam doloribus tenetur, perspiciatis qui id natus nam consectetur sapiente veritatis magnam maiores blanditiis exercitationem? Non, obcaecati?'
-        ]
-    ]]);
+    return view('blog',['header'=>'Blog', 'posts' => Post::all()]);
 });
 
 Route::get('/blog/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'memahami-quarter-life-crisis:-tantangan-dan-solusi',
-            'title' => 'Memahami Quarter Life Crisis: Tantangan dan Solusi',
-            'author' => 'Agus Pranyoto',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos harum, culpa porro fuga laboriosam doloribus tenetur, perspiciatis qui id natus nam consectetur sapiente veritatis magnam maiores blanditiis exercitationem? Non, obcaecati?'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'memahami-quarter-life-crisis:-tantangan-dan-solusi',
-            'title' => 'Memahami Quarter Life Crisis: Tantangan dan Solusi',
-            'author' => 'Agus Pranyoto',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos harum, culpa porro fuga laboriosam doloribus tenetur, perspiciatis qui id natus nam consectetur sapiente veritatis magnam maiores blanditiis exercitationem? Non, obcaecati?'
-        ]
-    ];
 
-    $post = Arr::first($posts, function($post) use ($slug){
+
+    $post = Arr::first(Post::all(), function($post) use ($slug){
         return $post['slug'] == $slug;
     });
 
     return view('post', ['header' => 'Detail Post', 'post' => $post]);
 
 });
-
